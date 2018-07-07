cf535cf3eeaad918dff360ebc30dd5e37ef71911
mayaflor <maya.r.rebelo@gmail.com>
2018-07-07 20:02:23 -0300

    counter and disable submit function work

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ var submit = document.querySelector("input[type=submit]");
 var input = document.querySelector("textarea");
 var countdownNumbers = parseInt(document.querySelector("span"));
 var textContent = input.value;
-var bt = document.getElementById("submit-check");
+var bt = document.getElementById("submit-check"); 
 bt.disabled = true;
 
-function manage(txt) {
+input.addEventListener("keyup",function() {
   bt.disabled = false;
-}    
+})
 
-function limite_textarea(valor) {
-  quant = 140;
-  total = valor.length;
-  if(total <= quant) {
-      rest = quant - total;
-      document.getElementById("count").innerHTML = rest;
-  } else {
-      document.getElementById("text").value = valor.substr(0,quant);
-  }
+function limit_textarea(valor) {
+   var quant = 140;
+   var total = valor.length;
+    if(total <= quant) {
+       var resto = quant - total;
+        document.getElementById("count").innerHTML = resto;
+    } else {
+        document.getElementById("text").value = valor.substr(0,quant);
+    }
 }
 
 submit.onclick = function(event) {
