ad7464ae54a1da662023bc124ef544693fae1de2
mayaflor <maya.r.rebelo@gmail.com>
2018-07-07 20:31:36 -0300

    counter restarts after submit button is pressed

--- script.js
+++ script.js
@@ -5,24 +5,26 @@ var textContent = input.value;
 var bt = document.getElementById("submit-check"); 
 bt.disabled = true;
 
+
 input.addEventListener("keyup",function() {
   bt.disabled = false;
 })
 
 function limit_textarea(valor) {
-   var quant = 140;
-   var total = valor.length;
-    if(total <= quant) {
-       var resto = quant - total;
-        document.getElementById("count").innerHTML = resto;
-    } else {
-        document.getElementById("text").value = valor.substr(0,quant);
-    }
+  var quant = 140;
+  var total = valor.length;
+  if (total <= quant) {
+      var resto = quant - total;
+      document.getElementById("count").innerHTML = resto;
+  } else {
+      document.getElementById("text").value = valor.substr(0,quant);
+  } 
 }
 
 submit.onclick = function(event) {
   var tweetBox = document.getElementsByClassName("tweet-box") [0];
   var newBox = document.createElement("div");
+  document.getElementById("count").innerHTML = 140;
   newBox.textContent = input.value;
   tweetBox.appendChild(newBox);
   input.value = "";
