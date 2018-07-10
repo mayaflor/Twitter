5b7031f7390e5b60c2f551f64958afd1383faefa
mayaflor <maya.r.rebelo@gmail.com>
2018-07-10 15:10:58 -0300

    last commit

--- script.js
+++ script.js
@@ -2,47 +2,44 @@ var submit = document.querySelector("input[type=submit]");
 var input = document.querySelector("textarea");
 var countdownNumbers = document.querySelector("span").innerHTML;
 var textContent = input.value;
+var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+var mybr = document.createElement('br');
 var bt = document.getElementById("submit-check"); 
 bt.disabled = true;
-
-
 input.addEventListener("keyup",function() {
   bt.disabled = false;
-})
-
+});
 function limit_textarea(valor) {
   var quant = 140;
   var total = valor.length;
   if (total <= quant) {
-      var resto = quant - total;
-      document.getElementById("count").innerHTML = resto;
+    var resto = quant - total;
+    document.getElementById("count").innerHTML = resto;
   } else {
-      document.getElementById("text").value = valor.substr(0,quant);
+    document.getElementById("text").value = valor.substr(0,quant);
   } 
   if (resto < 140 && resto > 19) {
-    document.getElementById('count').style.color = 'black';
+    document.getElementById("count").style.color = "black";
   }
   if (resto < 20 && resto > 11) {
-    document.getElementById('count').style.color = 'blue';
+    document.getElementById("count").style.color = "blue";
   }
   if (resto < 10 ) {
-      document.getElementById('count').style.color = 'red';
+    document.getElementById("count").style.color = "red";
   }
-}
-
+};
 submit.onclick = function(event) {
   var tweetBox = document.getElementsByClassName("tweet-box") [0];
   var newBox = document.createElement("div");
-  var linebreak = document.createElement('br');
   document.getElementById("count").innerHTML = 140;
-  newBox.textContent = input.value;
+  newBox.textContent = input.value + "  -  " + time;
   tweetBox.appendChild(newBox);
   input.value = "";
   event.preventDefault();
-}
-
+  console.log(time)
+};
 $(function() {
-  $(".txt-area").on('keyup paste', function() {
+  $(".txt-area").on("keyup paste", function() {
     var $el = $(this),
         offset = $el.innerHeight() - $el.height();
 
@@ -54,14 +51,4 @@ $(function() {
     }
   })
 });
-
-// function change_color() {
-//   console.log(countdownNumbers)
-//   if (countdownNumbers < 20 && countdownNumbers > 11) {
-//       document.getElementById('count').style.color = 'blue';
-//   }
-//   if (countdownNumbers < 10 ) {
-//       document.getElementById('count').style.color = 'red';
-//   }
-// };
 
