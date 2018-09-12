fdc3f071b7c5bc3f09c3bc9734ebd258ef90b31c
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-12 20:08:55 -0300

    into version 0.0.2

--- new-script.js
+++ new-script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+  $('#submit-check').prop('disabled', true);
 
   (function sendTweet() {
     $('#submit-check').click((event) => {
       $('#second-section').append(`<div class='tweets'>${$('textarea').val()}</div>`);
       $('textarea').val('');
-      
+    
       event.preventDefault();
     });
   }());
@@ -13,10 +14,25 @@ $(document).ready(function(){
     $('textarea').keyup(() => {
       const maxCharNum = 140;
       let rest = $('#count').text() - 1;
+      // $('#submit-check').prop('disabled', false);
+      // $('textarea').val() === '' ?
+      // $('#submit-check').prop('disabled', true) : $('#submit-check').prop('disabled', false);
 
       $('textarea').val().length <= maxCharNum ? 
       $('#count').text(rest) : $('textarea').val($('textarea').val().substr(0,maxCharNum));
-     });
+      
+      
+    });
   }());
+
+  (function disableButton() {
+    $('textarea').change(() => {
+      if ($('textarea').val() == '') {
+        $('#submit-check').prop('disabled', true);
+      } else {
+        $('#submit-check').prop('disabled', false);
+        }
+      });
+  }())
 
 });
