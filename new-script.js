e56a8193015c22c7e8eda49f579610ef5a701978
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-12 19:15:08 -0300

    counter working

--- new-script.js
+++ new-script.js
@@ -1,25 +1,22 @@
 $(document).ready(function(){
 
-  $('#submit-check').click((event) => {
-    $('#second-section').append(`<div class='tweets'>${$('textarea').val()}</div>`);
-    $('textarea').val('');
-    
-    event.preventDefault();
-  });
+  (function sendTweet() {
+    $('#submit-check').click((event) => {
+      $('#second-section').append(`<div class='tweets'>${$('textarea').val()}</div>`);
+      $('textarea').val('');
+      
+      event.preventDefault();
+    });
+  }());
 
-  $('textarea').keyup(() => {
-    const maxCharNum = 140;
-    let rest = $('#count').text() - $('textarea').val().length;
-    console.log(rest)
-    // $('textarea').val().length >= maxCharNum ?
-    // $('#count').text(rest) : $('textarea').val($('textarea').val().substr(0,maxCharNum));
+  (function tweetCounter() {
+    $('textarea').keyup(() => {
+      const maxCharNum = 140;
+      let rest = $('#count').text() - 1;
 
-    if ($('textarea').val().length >= maxCharNum) {
-      $('#count').text(rest)
-    } else {
-      $('textarea').val($('textarea').val().substr(0,maxCharNum));
-    }
-    
-  });
+      $('textarea').val().length <= maxCharNum ? 
+      $('#count').text(rest) : $('textarea').val($('textarea').val().substr(0,maxCharNum));
+     });
+  }());
 
 });
